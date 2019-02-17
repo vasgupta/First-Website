ba542235d91c3c87c48ef0cb01652ab7b1aa7afd
vasgupta <gupta.vasudev@gmail.com>
2019-02-16 23:35:31 -0500

    Add files via upload
    First version of the website

---
+++ get_info.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+Hello, <?php echo $_GET["fname"]; echo $_GET["lname"]; ?><br>
+    You live at <?php echo $_GET["address"]; ?> <br>
+    At price $<?php echo $_get["price"]; ?>
+</body>
+</html>
